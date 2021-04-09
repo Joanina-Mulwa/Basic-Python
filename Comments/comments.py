593a895ab7e2ea434939926e864d9179c3d55d92
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-09 21:43:36 +0300

    Initial Commit

---
+++ Comments/comments.py
@@ -0,0 +1,5 @@
+# this is a comment
+# in python, comments start with hashtag sign
+# python ignore the rest of line of code that start with this sign
+
+print("The above lines of code sow comments")
